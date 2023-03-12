6c2985d31dd21af575c181dbe4f2c87a5648536e
Robert Szczechura <szczechi@Roberts-MacBook-Pro.local>
2023-03-12 20:26:06 +0100

    add input and button, style input and button, add fuction to input and button

--- Main_page/index.js
+++ Main_page/index.js
@@ -1,10 +1,6 @@
-const cardTemplate = ` <div class="card">
-<img class="avatar" src="avatar.png" alt="avatar" />
-<div>
-  <h3 class="name">Szczechi</h3>
-  <p class="job">QA Tester</p>
-</div>
-</div>`;
+const input = document.querySelector(".search-bar");
+
+const button = document.querySelector(".button");
 
 const wrapper = document.querySelector(".app-container");
 
@@ -12,4 +8,25 @@ const main = () => {
   wrapper.innerHTML = cardTemplate;
 };
 
-main();
+const createCard = (text) => {
+  const cardTemplate = ` <div class="card">
+  <img class="avatar" src="avatar.png" alt="avatar" />
+  <div>
+    <h3 class="name">Szczechi</h3>
+    <p class="job">${text}</p>
+  </div>
+  </div>`;
+  wrapper.innerHTML = cardTemplate;
+};
+
+let text = "";
+
+input.addEventListener("input", (event) => {
+  text = event.target.value;
+  console.log(text);
+});
+
+button.addEventListener("click", (event) => {
+  createCard(text);
+  console.log(event.target);
+});
